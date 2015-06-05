d52e46cae2c2931aa6e7962d2ea188f73327ac56
tulanglaoli@foxmail.com <tulanglaoli@foxmail.com>
2015-06-05 17:24:47 +0800

    DTDmanager_add

---
+++ DTDManager/DTDManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTDManager
+{
+    public static class DTDManager
+    {
+
+        public static DTDBody FindBodybyName(List<DTDBody> db,string name)
+        {
+            foreach (DTDBody DB in db)
+            {
+                if (DB.Name == name)
+                {
+                    return DB;
+                }
+            }
+            return null;
+        }
+
+        public static DTDATTLISTItem FindElementByName(DTDBody db, string name)
+        {
+            foreach (DTDATTLISTItem dat in db.DTDAttList)
+            {
+                if(dat.Name == name)
+                {
+                    return dat;
+                }
+            }
+            return null;
+        }
+
+    }
+}
