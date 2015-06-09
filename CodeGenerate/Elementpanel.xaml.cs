bb5bbce97b0ffb11e9fdc9a9c14bf12e13d6824b
tulanglaoli@foxmail.com <tulanglaoli@foxmail.com>
2015-06-09 19:34:28 +0800

    Revert "WPF"
    This reverts commit d2362ded5591ade16d193f56586476efabf2d7a6.

--- CodeGenerate/Elementpanel.xaml.cs
+++ CodeGenerate/Elementpanel.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CodeGenerate
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Window1.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
